b24c4a60f5b255a2b02b241ce8a460cf45b04783
liudh <liudh>
2017-10-24 22:09:55 +0800

    工具类

---
+++ omni_server_common/src/main/java/com/dubbo/utils/Servlets.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2005-2012 springside.org.cn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.dubbo.utils;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * Http与Servlet工具类.
+ */
+public class Servlets {
+
+	/**
+	 * 获取当前请求对象
+	 * @return
+	 */
+	public static HttpServletRequest getRequest(){
+		try{
+			return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		}catch(Exception e){
+			return null;
+		}
+	}
+}
